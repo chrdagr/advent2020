fc8f31ead76ad25a382b41f66c33e61a25f5c6ff
Χριστιάνα Δ <christiana@MBPtoutristiana.lan>
2020-12-28 16:23:33 +0100

    Day 6

--- puz6.py
+++ puz6.py
@@ -3,7 +3,6 @@ with open("input6ex.txt") as file:
     for line in file: 
         line = line.strip() #or some other preprocessing
         lines.append(line) #storing everything in memory!
-   # print(lines)
 
 length = len(lines)
 print(length)
@@ -17,13 +16,12 @@ for i in range(length):
         k+=1
     else:
         tmp = ''.join(lines[(i-k):(i)])
-        print(tmp)
         items.append("".join(set(tmp)))
         k = 0
         count+=len("".join(set(tmp)))
-print(tmp)
+
 tmp = ''.join(lines[(i-k+1):(i+1)])
+
 items.append("".join(set(tmp)))
 count+=len("".join(set(tmp)))
-print(items)
 print(count)
--- puz6b.py
+++ puz6b.py
@@ -1,12 +1,10 @@
 lines = []
-with open("input6ex.txt") as file:
+with open("input6.txt") as file:
     for line in file: 
         line = line.strip() #or some other preprocessing
         lines.append(line) #storing everything in memory!
-    print(lines)
 
 length = len(lines)
-print(length)
 
 k=0
 items = []
@@ -17,7 +15,17 @@ for i in range(length):
         k+=1
     else:
         tmp = ''.join(lines[(i-k):(i)])
-        print(tmp)
-        items.append("".join(set(tmp)))
+        tmp2 = "".join(set(tmp))
+        items.append(tmp2)
+        for char in tmp2:
+            if (tmp.count(char) == k):
+                count+=1
         k = 0
-        count+=len("".join(set(tmp)))
+
+tmp = ''.join(lines[(i-k+1):(i+1)])
+tmp2 = "".join(set(tmp))
+items.append(tmp2)
+for char in tmp2:
+    if (tmp.count(char) == k):
+        count+=1
+print(count)
